90f7acdeaecf2dd717ddd901df831781617290df
Dongmin <21600397@handong.edu>
2017-10-30 15:04:56 +0900

    finished template

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
+
 const request = require('request');
-const argv = require('yargs').argv;
+const apiKey = '3fdffea70bf588e43b0293a1af2c734b';
 
-let apiKey = '3fdffea70bf588e43b0293a1af2c734b';
-let city = argv.c || 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 request(url, function (err, response, body) {
-  if(err){
-    console.log('error:', error);
-  } else {
-  	let weather = JSON.parse(body);
-    let message = `It's ${weather.main.temp} degrees in ${weather.name}!` ;
-    console.log(message);
-  }
-});
-
-
-
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
