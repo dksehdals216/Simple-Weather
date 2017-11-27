6ea3f45b9fa9fe7bc2bc9e5ba9475ee891e7d48c
Dongmin <21600397@handong.edu>
2017-11-27 17:53:07 +0900

    Added bootstrap and images

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const apiKey = '3fdffea70bf588e43b0293a1af2c734b';
 
 app.post('/', function (req, res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
 request(url, function (err, response, body) {
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again'});
@@ -13,7 +14,7 @@ request(url, function (err, response, body) {
       if(weather.main == undefined){
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!, and     `;
         res.render('index', {weather: weatherText, error: null});
       }
     }
