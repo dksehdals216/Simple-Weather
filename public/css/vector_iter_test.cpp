2f3c726dd39db281c902778b2e12102aaa63a72d
Dongmin <21600397@handong.edu>
2017-12-08 16:27:47 +0900

    Simplify code and finish up index file

---
+++ public/css/vector_iter_test.cpp
@@ -0,0 +1,14 @@
+// Create
+vector< vector<int>> vec(4, vector<int(4)>;
+// Write
+vec[2][3] = 10;
+// Read
+int a = vec[2][3];
+//insert
+use push_back or insert
+
+// You could also use iterators:
+
+    for (vector< vector<int> >::iterator it = vec.begin(); it != vec.end(); ++it) {
+        it->push_back(j); // Add column to all rows		
+}
